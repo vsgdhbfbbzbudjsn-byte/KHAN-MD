be097ffe8f4123874d3c82be206b2e5d0d2bbe0a
Jawad Tech <jawadtechxd@gmail.com>
2025-08-10 10:24:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 // add your Session Id
-USER_NAME: process.env.USER_NAME || "",
-// add username of your github acount for fork verification.
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
